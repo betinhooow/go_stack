c65fb679672aea736288b36700505fc8511ee4de
Roberto Nobre <robertonobre90@gmail.com>
2019-04-29 07:22:19 -0300

    go_node - users migration

--- node_js/go_barber/src/database/migrations/20190428144806-create-users.js
+++ node_js/go_barber/src/database/migrations/20190428144806-create-users.js
@@ -1,23 +1,48 @@
-'use strict';
-
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+    const UsersTable = queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING
+      },
+      avatar: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      password_hash: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      provider: {
+        allowNull: false,
+        defaultvalue: false,
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
 
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    return UsersTable
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+    return queryInterface => queryInterface.dropTable('users')
   }
-};
+}
