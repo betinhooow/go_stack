5cfdbb1a41dbbbe658866df33181237979cefc6c
Roberto Nobre <robertonobre90@gmail.com>
2019-04-29 06:39:03 -0300

    go_barber - views configs applied

--- node_js/go_barber/src/routes.js
+++ node_js/go_barber/src/routes.js
@@ -3,7 +3,7 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', (req, res) => {
-  return res.send('Salve')
+  return res.render('auth/signup')
 })
 
 module.exports = routes
