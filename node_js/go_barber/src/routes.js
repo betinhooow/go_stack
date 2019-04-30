740f7c614aac6d22f84f1e78108d061acc56460f
Roberto Nobre <robertonobre90@gmail.com>
2019-04-30 07:25:13 -0300

    go_node - uploud avatar user by multer

--- node_js/go_barber/src/routes.js
+++ node_js/go_barber/src/routes.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const multerConfig = require('./config/multer')
+const uploud = require('multer')(multerConfig)
 
 const routes = express.Router()
 
 const UserController = require('./app/controllers/UserController')
 
 routes.get('/signup', UserController.create)
-routes.post('/signup', UserController.store)
+routes.post('/signup', uploud.single('avatar'), UserController.store)
 
 module.exports = routes
