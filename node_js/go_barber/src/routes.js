124bb0268c4ec5cba7c320765a148cde111d3a9d
betinhoow <robertonobre90@gmail.com>
2019-09-03 06:30:35 -0300

    sucrase and nodemon configs

--- node_js/go_barber/src/routes.js
+++ node_js/go_barber/src/routes.js
@@ -1,9 +1,9 @@
-const { Router } = require ('express');
+import { Router } from 'express';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-    return res.json({ message: 'ok' });
+    return res.json({ message: 'ok1' });
 })
 
-module.exports = routes;
+export default routes;
