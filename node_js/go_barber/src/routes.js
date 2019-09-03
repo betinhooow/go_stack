7032e42fe8febe052a33c1cec18193c0c23032b3
betinhoow <robertonobre90@gmail.com>
2019-09-03 06:21:28 -0300

    Restart de desenvolvimento da aplicação, estrutura inicial

--- node_js/go_barber/src/routes.js
+++ node_js/go_barber/src/routes.js
@@ -1,34 +1,9 @@
-const express = require('express')
-const multerConfig = require('./config/multer')
-const uploud = require('multer')(multerConfig)
+const { Router } = require ('express');
 
-const routes = express.Router()
+const routes = new Router();
 
-const authMiddleware = require('./app/middlewares/auth')
-const guestMiddleware = require('./app/middlewares/guest')
-
-const UserController = require('./app/controllers/UserController')
-const SessionController = require('./app/controllers/SessionController')
-const DashboardController = require('./app/controllers/DashboardController')
-
-routes.use((req, res, next) => {
-  res.locals.flashSuccess = req.flash('success')
-  res.locals.flashError = req.flash('error')
-
-  return next()
+routes.get('/', (req, res) => {
+    return res.json({ message: 'ok' });
 })
 
-routes.get('/', guestMiddleware, SessionController.create)
-routes.post('/signin', SessionController.store)
-
-routes.get('/signup', guestMiddleware, UserController.create)
-routes.post('/signup', uploud.single('avatar'), UserController.store)
-
-// saying that all /app routes needs to hav a session to get access
-routes.use('/app', authMiddleware)
-
-routes.get('/logout', SessionController.destroy)
-
-routes.get('/app/dashboard', DashboardController.index)
-
-module.exports = routes
+module.exports = routes;
