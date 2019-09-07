be99ce38feb8729bb8bd9a55548dd73eec5816bb
betinhoow <robertonobre90@gmail.com>
2019-09-07 12:30:27 -0300

    Eslint, prettier and airbnb control code pattern

--- node_js/go_barber/src/routes.js
+++ node_js/go_barber/src/routes.js
@@ -2,8 +2,6 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ message: 'ok1' });
-})
+routes.get('/', (req, res) => res.json({ message: 'ok1' }));
 
 export default routes;
