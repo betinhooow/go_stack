36a4a57f01048a39cff14fdb1b116a3285283c48
betinhoow <robertonobre90@gmail.com>
2019-09-08 17:06:00 -0300

    Post users

--- node_js/go_barber/src/routes.js
+++ node_js/go_barber/src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Roberto Nobre',
-    email: 'rnobre.dev@cagada.com',
-    password_hash: '123456',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
