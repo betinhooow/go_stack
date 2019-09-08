7bf6a69d5fdaf5866b3f560004c262de0d0a25af
betinhoow <robertonobre90@gmail.com>
2019-09-08 16:52:01 -0300

    conecting at database

--- node_js/go_barber/src/database/migrations/20190907154018-create-users.js
+++ node_js/go_barber/src/database/migrations/20190907154018-create-users.js
@@ -29,7 +29,7 @@ module.exports = {
         type: Sequelize.DATE,
         allowNull: false,
       },
-      deleted_at: {
+      updated_at: {
         type: Sequelize.DATE,
         allowNull: false,
       },
