42bfcc66a24390c3242abe3b6a694bd597d9cfee
betinhoow <robertonobre90@gmail.com>
2019-09-26 23:45:50 -0300

    Middlewares

--- node_js/node_start/index.js
+++ node_js/node_start/index.js
@@ -8,33 +8,51 @@ server.use((req, res, next) => {
   next();
 });
 
+function checkUserExists(req, res, next){
+  if(!req.body.name){
+    res.status(400).json({ error: "User name is required" });
+  }
+
+  return next();
+}
+
+function checkUserInArray(req, res, next){
+  const user = users[req.params.id];
+
+  if(!user){
+    return res.status(400).json({ error: "User does not exist" });
+  }
+
+  req.user = user;
+
+  next();
+}
+
 const users = ["Roberto", "Maria", "Felipe", "Gabriel"];
 
 server.get('/user', (req, res) => {
   return res.json(users)
 });
 
-server.get('/user/:id', (req, res) => {
-  const { id } = req.params;
-
-  return res.json(users[id]);
+server.get('/user/:id', checkUserInArray, (req, res) => {
+  return res.json(req.user);
 });
 
-server.post('/user', (req, res) => {
+server.post('/user', checkUserExists, (req, res) => {
   const { name } = req.body;
   users.push(name);
 
   return res.json(req.body);
 });
 
-server.put('/user/:id', (req, res) => {
+server.put('/user/:id', checkUserInArray, checkUserExists, (req, res) => {
   const { id } = req.params;
   users[id] = req.body.name;
 
   return res.json(users[id]); 
 });
 
-server.delete('/user/:id', (req, res) => {
+server.delete('/user/:id', checkUserInArray, (req, res) => {
   const { id } = req.params;
   users.splice(id, 1);
 
